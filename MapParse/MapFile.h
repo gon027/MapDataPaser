da2fb29ad28a82f7bf0ba35adcc7b791912501fd
Takashi <gon27.tk08@gmail.com>
2021-01-30 14:16:17 +0900

    テキストファイルを読み込んで解析できるようにした

--- MapParse/MapFile.h
+++ MapParse/MapFile.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <string>
 #include <fstream>
+#include <iostream>
 
 namespace MapParser {
 
-	inline std::string readMapFile() {
-		std::fstream file("test_matdata.txt");
+	inline std::string readMapFile(const std::string& _file) {
+		std::fstream file(_file);
 		if (!file) {
-			//std::cout << "Read Error" << std::endl;
+			std::cout << "Read Error" << std::endl;
 		}
 
 		std::string str;
 		std::string line;
 		while (std::getline(file, line)) {
+			line.erase(std::remove(line.begin(), line.end(), '	'), line.end());
+			line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
 			str += line;
 		}
 
--- MapParse/MapParse.cpp
+++ MapParse/MapParse.cpp
@@ -6,6 +6,7 @@
 #include "Token.h"
 #include "RegexToken.h"
 #include "MapData.h"
+#include "MapFile.h"
 using namespace std;
 
 MapParser::MapID getMapID(MapParser::RegexToken& _rt) {
@@ -191,13 +192,19 @@ MapParser::MapData parseMapData(MapParser::RegexToken& _rt) {
 }
 
 int main()
-{	
+{
 	//MapPaser::RegexToken rt{ "{mapWide=[width=10,height=11]}" };
 	//MapPaser::RegexToken rt{ "{mapWide={0, 2, 10, 100, 1000, 99}}" };
 	//MapPaser::RegexToken rt{ "{mapObject={[name=10,x=120,y=130,width=300,height=499],[name=20,x=120,y=130,width=300,height=499],[name=20,x=12220,y=13220,width=32200,height=49229]}}" };
-	MapParser::RegexToken rt{ 
+	/*MapParser::RegexToken rt{
 		"{[mapID=1,mapWide={[width=100,height=100]},mapData={560,2,10,100,1000,99,0,0,0,9},mapObject={[name=123,x=12,y=123,width=90,height=948],[name=123,x=12,y=123,width=90,height=948]}]}"
 	};
+	*/
+
+	auto a = MapParser::readMapFile("Test.txt");
+	cout << a << endl;
+
+	MapParser::RegexToken rt{ a };
 
 	while (rt.isEof()) {
 		auto t = rt.getToken();
